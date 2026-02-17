919cd6da367b3e7d04d923f7c1760aca91081be5
Theoneandonlyajcool <ac1106508@gmail.com>
2026-02-17 16:49:23 +0100

    Created the chat bot UI and implemented quick replies

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.ComponentProps<"textarea">
+>(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      className={cn(
+        "flex min-h-[80px] bg-black text-white w-full rounded-md border border-[#1A1A2E] outline-none px-3 py-2 text-base  placeholder:text-muted-foreground  md:text-sm",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Textarea.displayName = "Textarea";
+
+export { Textarea };
